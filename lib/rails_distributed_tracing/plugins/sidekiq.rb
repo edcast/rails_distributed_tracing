648157a88bb4cc9563cbee472b7346e0747e62c8
Ranjeet Maurya <ranjeet@edcast.com>
2020-06-07 12:56:52 +0530

    enabled tagged login

--- lib/rails_distributed_tracing/plugins/sidekiq.rb
+++ lib/rails_distributed_tracing/plugins/sidekiq.rb
@@ -1,15 +1,9 @@
 module DistributedTracing
   module SidekiqMiddleware
     class Client
-      def call(worker_class, job, queue, redis_pool)
-        tag = "#{worker_class.to_s} #{SecureRandom.hex(12)}"
-        ::Rails.logger.tagged(tag) do
-          job_info = "Start at11 #{Time.now.to_default_s}: #{job.inspect}"
-          ::Rails.logger.info(job_info)
-          yield
-        end        
-        #job[DistributedTracing::TRACE_ID] = trace_id
-        #yield
+      def call(worker_class, job, queue, redis_pool)      
+        job[DistributedTracing::TRACE_ID] = trace_id
+        yield
       end
 
       private
@@ -22,7 +16,7 @@ module DistributedTracing
       def call(worker, job, queue)
         logger = worker.logger
 
-        if logger.respond_to?(:tagged)
+        if true
           DistributedTracing.trace_id = job[DistributedTracing::TRACE_ID]
           logger.tagged(DistributedTracing.trace_id) {yield}
           DistributedTracing.trace_id= nil
